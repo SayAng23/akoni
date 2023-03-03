cab458d3868b166d2d929878f32f3d87319ab066
SayAng23 <saysonangelo97@gmail.com>
2023-03-03 11:12:19 +0800

    git add new to github

---
+++ app/Models/menu.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+class menu{
+
+    public static function all(){
+        $product_coffee = [
+            [
+                'id'            =>  '1',
+                'lead'          =>  'menu',
+                'heading'       =>  'Starters',
+                'food_image'    =>  'assets/img/menu/menu-item-1.png',
+                'desc'          =>  'Magnam Tiste
+                Lorem, deren, trataro, filede, nerada
+                
+                $5.95'
+            ],
+        ];
+
+        return($product_coffee);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'index'])->name('UI.atubangan');
